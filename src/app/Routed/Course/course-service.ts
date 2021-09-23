ffd615df931684f7c305d2d91070b8e14cbd5e77
omid5643 <seyed.abaei@yahoo.com>
2021-09-23 13:08:34 -0500

    done update course

--- src/app/Routed/Course/course-service.ts
+++ src/app/Routed/Course/course-service.ts
@@ -7,36 +7,47 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class CourseService {
 
-    constructor(private http: HttpClient, private modalService: ModalService) {
+  constructor(private http: HttpClient, private modalService: ModalService) {
 
 
 
+  }
+  CreateCourse(course: Course) {
+    let postCourseUrl = 'http://localhost:59019/api/Course/Add';
+    this.http.post(postCourseUrl, course).subscribe()
+
+    error => {
+      this.modalService.ShowModal()
     }
-    CreateCourse(course:Course) {
-        let postCourseUrl = 'http://localhost:59019/api/Course/Add';
-        this.http.post(postCourseUrl,course).subscribe()
+  }
 
-        error => {
-            this.modalService.ShowModal()
-        }
+
+  GetCourses(): Observable<Course[]> {
+    let getCourseUrl = 'http://localhost:59019/api/Course/Load';
+    return this.http.get<Course[]>(getCourseUrl);
+
+
+  }
+  RemoveCourse(id:number): Observable<boolean> {
+    let Url = 'http://localhost:59019/api/Course/Remove';
+
+    let body = new HttpParams();
+    body = body.set('id', id);
+
+    return this.http.post(Url, body) as Observable<boolean>;
+
+  }
+
+  UpdateCourse(course:Course) {
+    let Url = 'http://localhost:59019/api/Course/Update';
+
+    return this.http.post(Url,course).subscribe();
+
+    error => {
+      this.modalService.ShowModal()
     }
 
-
-    GetCourses():Observable<Course[]> {
-        let getCourseUrl = 'http://localhost:59019/api/Course/Load';
-      return  this.http.get<Course[]>(getCourseUrl);
-
-
-    }
-    RemoveCourse(id:number):Observable<boolean>{
-        let Url = 'http://localhost:59019/api/Course/Remove';
-       
-        let body = new HttpParams();
-        body = body.set('id', id);
-
-      return this.http.post(Url,body) as Observable<boolean>;
-
-    }
+  }
 
 
 
--- src/app/Routed/Course/get-courses.component.ts
+++ src/app/Routed/Course/get-courses.component.ts
@@ -1,21 +1,28 @@
 //ran ng update to migrate
-import { Component, OnInit } from '@angular/core';
+
 import { ModalService } from '../../services/modal-service';
 import { CourseService } from './course-service';
+import { Course } from '../../models/course';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-get-courses',
   templateUrl: './get-courses.component.html',
-  styleUrls: ['./get-courses.component.less']
+  styleUrls: ['./get-courses.component.less'],
+  changeDetection:ChangeDetectionStrategy.OnPush
+
 })
 export class GetCoursesComponent implements OnInit {
   Title: string;
-  Courses: any;
+  Courses:Course[];
+  CourseToUpdate:Course;
 
 
   constructor(private courseService: CourseService, private modalService: ModalService) {
 
     this.Title = "Course List Page";
+    this.CourseToUpdate=new Course;
+
   }
 
   ngOnInit() {
@@ -37,6 +44,23 @@ export class GetCoursesComponent implements OnInit {
 
     });
   }
+
+  updateCourse(){
+
+    this.courseService.UpdateCourse(this.CourseToUpdate);
+    this.getCourses();
+  }
+
+  
+  showUpdateModal(id:number)
+ {
+  
+     let foundCourse=this.Courses.find(x=>x.Id===id);
+
+     this.CourseToUpdate=foundCourse;
+      document.getElementById("openUpdateModalButton").click();
+ }
+
 
 
 
