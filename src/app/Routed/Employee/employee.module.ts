bec68dbc3f9414ed1cc56c94f8dbf4052ba05fee
omid5643 <seyed.abaei@yahoo.com>
2022-03-15 10:42:14 -0500

    done

--- src/app/Routed/Employee/employee.module.ts
+++ src/app/Routed/Employee/employee.module.ts
@@ -1,39 +1,25 @@
-
-
-import {NgModule,Injector} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';//<==lazy laded module still need to import them eventhough imported in app module
-import {LogModule} from '../../modules/logModule/log.module';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ListEmployeesComponent} from './list-employees.component';
-import {CreateEmployeeComponent} from './create-employee.component';
-import { BannerComponent } from '../../components/banner/banner.component';
-import {StepComponent} from '../../components/step/step.component';
-import {WizardStep} from '../../directives/wizardstep';
-import {ToLower} from '../../pipes/toLower.pipe';
-import {CreateEmployeeFormCanDeactivateService} from '../Employee/createEmployeeCanDeactivate.service';
-const routes: Routes = [{path: '', component: ListEmployeesComponent},
- { path: 'CreateEmployee', component: CreateEmployeeComponent, canDeactivate:[CreateEmployeeFormCanDeactivateService]}
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CreateEmployeeComponent } from './create-employee.component';
+import { GetEmployeesComponent } from './get-employees.component';
+
+
+
+
+const routes: Routes = [{ path: '', component: GetEmployeesComponent },
+{ path: 'CreateEmployee', component: CreateEmployeeComponent }
 ];
 
-// export function GetLogger( ):Logger
-// {
-//    let _logger=new Logger(self..get('logtypetoken'));
-//    return _logger;
-// }
-
-@NgModule(
-    {
-imports: [RouterModule.forChild(routes) , FormsModule,LogModule.WithLogTypes('alert'), ReactiveFormsModule, CommonModule],
- declarations: [ CreateEmployeeComponent,ListEmployeesComponent,BannerComponent,StepComponent,WizardStep,ToLower],
-
- providers:[CreateEmployeeFormCanDeactivateService]
+@NgModule({
+  declarations: [
+    CreateEmployeeComponent, 
+    GetEmployeesComponent
+  ],
+  imports: [FormsModule,
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
 })
-export class EmployeeModule {
-    constructor(injector: Injector) {
-     
-    }
-    
-
-
-}
+export class EmployeeModule { }
