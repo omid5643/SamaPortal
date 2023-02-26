e5eb71f7af6ca5ab85f5e704a27515e3efbcebda
Seyed Abaei <Seyed.Abaei@us.loomis.com>
2023-02-26 15:27:55 -0600

    fix: typo

--- src/app/Routed/Employee/get-employees.component.ts
+++ src/app/Routed/Employee/get-employees.component.ts
@@ -10,7 +10,7 @@ import { employeeType } from "../../Enums/EmployeeType";
   @Component({
     selector: 'app-get-employees',
     templateUrl:'./get-employees.component.html',
-    styleUrls: ['./get-employees.componet.less']
+    styleUrls: ['./get-employees.component.less']
   
   })
 
